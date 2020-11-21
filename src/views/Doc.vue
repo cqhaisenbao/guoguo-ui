b2f14ea9b24c760c01107b2c9993eebdc2e5506b
cqhaisenbao <fanyu9353@gmail.com>
2020-11-21 17:05:52 +0800

    初始化Doc页面html和css

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,40 @@
 <template>
-    <div>
-        <Topnav/>
+    <div class="layout">
+        <Topnav toggleMenuButtonVisible class="nav" />
+        <div class="content">
+            <aside>
+                <h2>文档</h2>
+                <ol>
+                    <li>
+                        <router-link to="/doc/intro">介绍</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/install">安装</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/get-started">开始使用</router-link>
+                    </li>
+                </ol>
+                <h2>组件列表</h2>
+                <ol>
+                    <li>
+                        <router-link to="/doc/switch">Switch 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/button">Button 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/dialog">Dialog 组件</router-link>
+                    </li>
+                    <li>
+                        <router-link to="/doc/tabs">Tabs 组件</router-link>
+                    </li>
+                </ol>
+            </aside>
+            <main>
+                <router-view />
+            </main>
+        </div>
     </div>
 </template>
 
