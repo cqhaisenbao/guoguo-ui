e4015cfbd270affbcc1bdfd196e88c672fd43399
cqhaisenbao <fanyu9353@gmail.com>
2020-11-22 17:31:09 +0800

    支持size属性
    Signed-off-by: cqhaisenbao <fanyu9353@gmail.com>

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,16 +1,32 @@
 <template>
-    <button class="guoguo-button" :class="`guoguo-theme-${theme}`">
+    <button class="guoguo-button" :class="classes">
         <slot/>
     </button>
 </template>
 
 <script lang="ts">
+    import {computed} from 'vue';
+
     export default {
         props: {
             theme: {
                 types: String,
                 default: 'button'
+            },
+            size: {
+                type: String,
+                default: 'normal'
             }
+        },
+        setup(props) {
+            const {theme, size} = props;
+            const classes = computed(() => {
+                return {
+                    [`guoguo-theme-${ theme }`]: theme,
+                    [`guoguo-size-${ size }`]: size
+                };
+            });
+            return {classes};
         }
     };
 </script>
@@ -38,63 +54,77 @@
         border-radius: $radius;
         box-shadow: 0 1px 0 fade-out(black, 0.95);
         transition: background 250ms;
+
         & + & {
             margin-left: 8px;
         }
+
         &:hover,
         &:focus {
             color: $blue;
             border-color: $blue;
         }
+
         &:focus {
             outline: none;
         }
+
         &::-moz-focus-inner {
             border: 0;
         }
+
         &.guoguo-theme-link {
             border-color: transparent;
             box-shadow: none;
             color: $blue;
+
             &:hover,
             &:focus {
                 color: lighten($blue, 10%);
             }
         }
+
         &.guoguo-theme-text {
             border-color: transparent;
             box-shadow: none;
             color: inherit;
+
             &:hover,
             &:focus {
                 background: darken(white, 5%);
             }
         }
+
         &.guoguo-size-big {
             font-size: 24px;
             height: 48px;
             padding: 0 16px;
         }
+
         &.guoguo-size-small {
             font-size: 12px;
             height: 20px;
             padding: 0 4px;
         }
+
         &.guoguo-theme-button {
             &.guoguo-level-main {
                 background: $blue;
                 color: white;
                 border-color: $blue;
+
                 &:hover,
                 &:focus {
                     background: darken($blue, 10%);
                     border-color: darken($blue, 10%);
                 }
             }
+
             &.guoguo-level-danger {
                 background: $red;
                 border-color: $red;
                 color: white;
+
                 &:hover,
                 &:focus {
                     background: darken($red, 10%);
@@ -102,47 +132,57 @@
                 }
             }
         }
+
         &.guoguo-theme-link {
             &.guoguo-level-danger {
                 color: $red;
+
                 &:hover,
                 &:focus {
                     color: darken($red, 10%);
                 }
             }
         }
+
         &.guoguo-theme-text {
             &.guoguo-level-main {
                 color: $blue;
+
                 &:hover,
                 &:focus {
                     color: darken($blue, 10%);
                 }
             }
+
             &.guoguo-level-danger {
                 color: $red;
+
                 &:hover,
                 &:focus {
                     color: darken($red, 10%);
                 }
             }
         }
+
         &.guoguo-theme-button {
             &[disabled] {
                 cursor: not-allowed;
                 color: $grey;
+
                 &:hover {
                     border-color: $grey;
                 }
             }
         }
+
         &.guoguo-theme-link, &.guoguo-theme-text {
             &[disabled] {
                 cursor: not-allowed;
                 color: $grey;
             }
         }
-        > .guoguo-loadingIndicator{
+
+        > .guoguo-loadingIndicator {
             width: 14px;
             height: 14px;
             display: inline-block;
@@ -154,8 +194,13 @@
             animation: guoguo-spin 1s infinite linear;
         }
     }
+
     @keyframes guoguo-spin {
-        0%{transform: rotate(0deg)}
-        100%{transform: rotate(360deg)}
+        0% {
+            transform: rotate(0deg)
+        }
+        100% {
+            transform: rotate(360deg)
+        }
     }
 </style>
