e5cdf99b7044c3f11d5f6f7d990cfa5cac4de6b4
cqhaisenbao <fanyu9353@gmail.com>
2020-11-24 11:51:59 +0800

    完成tabs动画切换
    Signed-off-by: cqhaisenbao <fanyu9353@gmail.com>

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,29 +1,103 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-        <component v-for="(c,index) in defaults" :is="c" :key="index"/>
+    <div class="guoguo-tabs">
+        <div class="guoguo-tabs-nav" ref="container">
+            <div class="guoguo-tabs-nav-item" v-for="(t,index) in titles"
+                 :ref="el => { if (t===selected) selectedItem = el }"
+                 @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}
+            </div>
+            <div class="guoguo-tabs-nav-indicator" ref="indicator"></div>
+        </div>
+        <div class="guoguo-tabs-content">
+            <component class="guoguo-tabs-content-item" :class="{selected: c.props.title === selected }"
+                       v-for="c in defaults" :is="c"/>
+
+        </div>
     </div>
 </template>
 
 <script lang="ts">
     import Tab from './Tab.vue';
+    import {computed, ref, onMounted, onUpdated} from 'vue';
 
     export default {
+        props: {
+            selected: {
+                type: String
+            }
+        },
         setup(props, context) {
+            const selectedItem = ref<HTMLDivElement>(null);
+            const indicator = ref<HTMLDivElement>(null);
+            const container = ref<HTMLDivElement>(null);
+            const x = () => {
+                const {width} = selectedItem.value.getBoundingClientRect();
+                indicator.value.style.width = width + 'px';
+                const {left: left1} = container.value.getBoundingClientRect();
+                const {left: left2} = selectedItem.value.getBoundingClientRect();
+                const left = left2 - left1;
+                indicator.value.style.left = left + 'px';
+            };
+            onMounted(x);
+            onUpdated(x);
+            //拿到当前组件作用域内子组件
             const defaults = context.slots.default();
             defaults.forEach((tag) => {
                 if (tag.type !== Tab) {
                     throw new Error('Tabs子标签必须是Tab');
                 }
             });
+            const current = computed(() => {
+                return defaults.find(tag =>
+                    tag.props.title === props.selected);
+            });
+
             const titles = defaults.map((tag) => {
                 return tag.props.title;
             });
-            return {defaults, titles};
+            const select = (title: string) => {context.emit('update:selected', title);};
+            return {defaults, titles, current, select, selectedItem, indicator, container};
         }
     };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+    $blue: #40a9ff;
+    $color: #333;
+    $border-color: #d9d9d9;
+    .guoguo-tabs {
+        &-nav {
+            display: flex;
+            color: $color;
+            border-bottom: 1px solid $border-color;
+            position: relative;
 
+            &-item {
+                padding: 8px 0;
+                margin: 0 16px;
+                cursor: pointer;
+
+                &:first-child {
+                    margin-left: 0;
+                }
+
+                &.selected {
+                    color: $blue;
+                }
+            }
+
+            &-indicator {
+                position: absolute;
+                height: 3px;
+                background: $blue;
+                left: 0;
+                bottom: -1px;
+                width: 100px;
+                transition: all 250ms;
+            }
+        }
+
+        &-content {
+            padding: 8px 0;
+        }
+    }
 </style>
