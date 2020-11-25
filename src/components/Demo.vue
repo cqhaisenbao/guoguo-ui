cc561a098b6217692ea6b46d2f2ef208b7f9b0e4
cqhaisenbao <fanyu9353@gmail.com>
2020-11-25 19:30:35 +0800

    展示Demo标题
    Signed-off-by: cqhaisenbao <fanyu9353@gmail.com>

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,41 +1,31 @@
 <template>
-    <div>
-        <h1>Switch 组件示例 </h1>
-        <Demo :component="Switch1Demo"/>
-        <Demo :component="Switch2Demo"/>
+    <div class="demo">
+        <h2>{{component.__sourceCodeTitle}} </h2>
+        <div class="demo-component">
+            <component :is="component"/>
+        </div>
+        <div class="demo-actions">
+            <Button>查看代码</Button>
+        </div>
+        <div class="demo-code">
+                <pre class="language-html"
+                     v-html="Prism.highlight(component.__sourceCode, Prism.languages.html, 'html')"/>
+        </div>
     </div>
 </template>
 
 <script lang="ts">
-    import Switch from '../lib/Switch.vue';
-    import Switch1Demo from './Switch1.demo.vue';
-    import Switch2Demo from './Switch2.demo.vue';
     import Button from '../lib/Button.vue';
     import 'prismjs';
     import 'prismjs/themes/prism.css';
 
     const Prism = (window as any).Prism;
-
-    import {ref} from 'vue';
-    import Demo from './Demo.vue';
-
-    console.log(Switch1Demo.__sourceCode);
-
     export default {
-        components: {
-            Demo,
-            Button
+        props: {
+            component: Object
         },
-        setup() {
-            const bool = ref(false);
-            return {
-                bool,
-                Switch2Demo,
-                Switch1Demo,
-                Button,
-                Switch,
-                Prism
-            };
+        setup(){
+            return{Prism,Button}
         }
     };
 </script>
